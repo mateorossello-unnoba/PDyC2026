aced65658f5f5ed19823acb2547f8014f7df5e9f
mateorossello-unnoba <143357472+mateorossello-unnoba@users.noreply.github.com>
2026-06-17 00:38:11 -0300

    fix: corregir errores menores

--- src/main/java/ar/edu/unnoba/greaterevents/repositories/NotificationRepository.java
+++ src/main/java/ar/edu/unnoba/greaterevents/repositories/NotificationRepository.java
@@ -3,9 +3,7 @@ package ar.edu.unnoba.greaterevents.repositories;
 import ar.edu.unnoba.greaterevents.models.notification.Notification;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByUserUsernameOrderByCreatedAtDesc(String username);
 }
