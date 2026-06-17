80a36c82cc3fc65ac9d2a3cda3353d43aade9937
mateorossello-unnoba <143357472+mateorossello-unnoba@users.noreply.github.com>
2026-06-17 00:38:11 -0300

    fix: corregir errores menores

--- src/main/java/ar/edu/unnoba/greaterevents/repositories/UserRepository.java
+++ src/main/java/ar/edu/unnoba/greaterevents/repositories/UserRepository.java
@@ -5,9 +5,7 @@ import java.util.Optional;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsernameIgnoreCase(String username);
     Optional<User> findByEmail(String email);
